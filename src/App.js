e15545a3d1ab75a96ffda69a30e4bfca1e6b6a46
Brendan Murty <brendan.murty@4mation.com.au>
2019-03-04 12:52:33 +1100

    Migrate to React Router
    https://reacttraining.com/react-router/web/example/basic

--- src/App.js
+++ src/App.js
@@ -2,32 +2,82 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <nav className="App-nav">
-            <ul>
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-            </ul>
-          </nav>
-        </header>
-        <main className="App-main">
-            <h1>Page Name</h1>
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-            <ul>
-                <li>One</li>
-                <li>Two</li>
-                <li>Three</li>
-            </ul>              
-        </main>
+function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">All</Link>
+          </li>
+          <li>
+            <Link to="/unread">Unread</Link>
+          </li>
+          <li>
+            <Link to="/tags">Tags</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={All} />
+        <Route path="/unread" component={Unread} />
+        <Route path="/tags" component={Tags} />
       </div>
-    );
-  }
+    </Router>
+  );
+}
+
+function All() {
+  return (
+    <div>
+      <h2>All Bookmarks</h2>
+    </div>
+  );
+}
+
+function Unread() {
+  return (
+    <div>
+      <h2>Unread</h2>
+    </div>
+  );
+}
+
+function Tags({ match }) {
+  return (
+    <div>
+      <h2>Tags</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/example1`}>Example 1</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/example2`}>Example 2</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/example3`}>Example 3</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:topicId`} component={Tag} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
+  );
+}
+
+function Tag({ match }) {
+  return (
+    <div>
+      <h3>Tag: {match.params.topicId}</h3>
+    </div>
+  );
 }
 
 export default App;
