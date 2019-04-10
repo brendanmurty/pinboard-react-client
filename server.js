06176c237019bd253eb4e64d4f89a669a12069de
Brendan Murty <b@murty.io>
2019-04-10 13:04:52 +1000

    Improved environment variable naming to suit current Create React App features
    https://facebook.github.io/create-react-app/docs/adding-custom-environment-variables

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const app_domain = process.env.APP_DOMAIN || 'localhost';
-const app_port = process.env.APP_PORT || 80;
+const app_domain = process.env.REACT_APP_DOMAIN || 'localhost';
+const app_port = process.env.REACT_APP_PORT || 80;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
--- src/App.js
+++ src/App.js
@@ -1,87 +1,56 @@
 import React from 'react';
+
 import Logo from './logo.svg';
 import './App.css';
 
+// import Pinboard from "node-pinboard";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <nav>
-        <header>
-          <img src={Logo} alt="Pinboard Client" />
-        </header>
+    console.log('Current Pinboard API Token: ' + process.env.REACT_APP_PINBOARD_API_TOKEN);
 
-        <ul>
-          <li>
-            <Link to="/">All</Link>
-          </li>
-          <li>
-            <Link to="/unread">Unread</Link>
-          </li>
-          <li>
-            <Link to="/tags">Tags</Link>
-          </li>
-        </ul>
+    return (
+        <Router>
+            <nav>
+                <header>
+                    <img src={Logo} alt="Pinboard Client" />
+                </header>
 
-        <hr />
+                <ul>
+                    <li>
+                        <Link to="/">All</Link>
+                    </li>
+                    <li>
+                        <Link to="/unread">Unread</Link>
+                    </li>
+                    <li>
+                        <Link to="/tags">Tags</Link>
+                    </li>
+                </ul>
 
-        <Route exact path="/" component={All} />
-        <Route path="/unread" component={Unread} />
-        <Route path="/tags" component={Tags} />
-      </nav>
-    </Router>
-  );
+                <hr />
+
+                <Route exact path="/" component={All} />
+                <Route path="/unread" component={Unread} />
+            </nav>
+        </Router>
+    );
 }
 
 function All() {
-  return (
-    <div>
-      <h2>All</h2>
-    </div>
-  );
+    return (
+        <div>
+            <h2>All</h2>
+        </div>
+    );
 }
 
 function Unread() {
-  return (
-    <div>
-      <h2>Unread</h2>
-    </div>
-  );
-}
-
-function Tags({ match }) {
-  return (
-    <div>
-      <h2>Tags</h2>
-      <ul>
-        <li>
-          <Link to={`${match.url}/example1`}>Example 1</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/example2`}>Example 2</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/example3`}>Example 3</Link>
-        </li>
-      </ul>
-
-      <Route path={`${match.path}/:topicId`} component={Tag} />
-      <Route
-        exact
-        path={match.path}
-        render={() => ''}
-      />
-    </div>
-  );
-}
-
-function Tag({ match }) {
-  return (
-    <div>
-      <h3>Tag: {match.params.topicId}</h3>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Unread</h2>
+        </div>
+    );
 }
 
 export default App;
