c66ed887052600ce94ca469a7e595019b784c1a1
Brendan Murty <b@murty.io>
2019-04-14 14:41:09 +1000

    Fix the Pinboard Recent API process

--- src/api/pinboard_recent.js
+++ src/api/pinboard_recent.js
@@ -1,12 +1,12 @@
-var pinboard = require('node-pinboard'),
-    pinboard_api = new pinboard(process.env.REACT_APP_PINBOARD_API_TOKEN);
+// API: /api/pinboard_recent
+
+var node_pinboard = require('node-pinboard'),
+    pinboard_api = new node_pinboard(process.env.PINBOARD_API_TOKEN);
 
 exports.get = function (request, response) {
     pinboard_api.recent({}, function(api_error, api_response) {
-        if (!api_error) {
-            response.writeHead(200, { 'Content-Type': 'application/json' });
-            response.write(api_response);
-            response.end();
+        if (!api_error && api_response != []) {
+            response.end(JSON.stringify(api_response.posts));
         }
     });
 };
