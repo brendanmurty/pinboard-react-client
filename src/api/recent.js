4467b50775f36dd865936cd89e0b59519fa29b0e
Brendan Murty <brendan@murty.au>
2023-02-01 14:33:55 +1100

    Remove obsolete HashID package

--- src/api/recent.js
+++ src/api/recent.js
@@ -1,19 +1,12 @@
 // API: /api/recent - Returns a JSON string of the user's most recent posts
 
 var node_pinboard = require('node-pinboard').default,
-  pinboard_api = new node_pinboard(process.env.PINBOARD_API_TOKEN),
-  hashids = require('hashids'),
-  hash_ids = new hashids();
+  pinboard_api = new node_pinboard(process.env.PINBOARD_API_TOKEN);
 
 exports.get = function (request, response) {
   pinboard_api.recent({}, function(api_error, api_response) {
     if (!api_error && api_response != []) {
       var recent_bookmarks = api_response.posts;
-
-      // Add a HashID to each bookmark item so it can be used for the "Edit" link in the bookmark list
-      recent_bookmarks.forEach(function(bookmark) {
-        bookmark.hashid = hash_ids.encode(bookmark.href);
-      });
 
       response.end(JSON.stringify(recent_bookmarks));
     }
--- src/api/unread.js
+++ src/api/unread.js
@@ -1,9 +1,7 @@
 // API: /api/unread - Returns a JSON string of the user's unread posts
 
 var node_pinboard = require('node-pinboard').default,
-  pinboard_api = new node_pinboard(process.env.PINBOARD_API_TOKEN),
-  hashids = require('hashids'),
-  hash_ids = new hashids();
+  pinboard_api = new node_pinboard(process.env.PINBOARD_API_TOKEN);
 
 exports.get = function (request, response) {
   pinboard_api.all({}, function(api_error, api_response) {
@@ -16,14 +14,6 @@ exports.get = function (request, response) {
 
         // Include only unread bookmarks
         unread_bookmarks = [];
-        api_response.forEach (function(bookmark) {
-          if (bookmark.toread == 'yes') {
-            // Add a HashID to each bookmark item so it can be used for the "Edit" link in the bookmark list
-            bookmark.hashid = hash_ids.encode(bookmark.href);
-
-            unread_bookmarks.push(bookmark);
-          }
-        });
 
         response.end(JSON.stringify(unread_bookmarks));
       }
