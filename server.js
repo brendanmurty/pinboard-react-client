57b6969b6f81201317148f2403ba15bf8a1f6f44
Brendan Murty <brendan@murty.au>
2023-02-06 20:19:44 +1100

    Add a feature to enable all CORS requests

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var express = require('express'),
   router = express.Router(),
   app = express();
 
+// Enable all CORS requests if needed
+if (process.env.PINBOARD_APP_ALLOW_ALL_CORS_REQUESTS) {
+  var cors = require('cors');
+  app.use(cors());
+
+  console.log('WARNING: All other domains can access data from this system! To disable this, remove the "PINBOARD_APP_ALLOW_ALL_CORS_REQUESTS" line from ".env"');
+}
+
 // Set the application domain and port for the server to listen on
 const app_domain = process.env.PINBOARD_APP_DOMAIN || 'localhost';
 const app_port = process.env.PINBOARD_APP_PORT || 8888;
@@ -26,4 +34,4 @@ app.get('*', function (request, response) {
 
 // Start the web server and listen for requests on the specified domain and port
 app.listen(app_port, app_domain);
-console.log('http server started at http://' + app_domain + ':' + app_port);
+console.log('Server started at http://' + app_domain + ':' + app_port);
